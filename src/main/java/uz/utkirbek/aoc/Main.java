213173002e627015dd5c634db1c0331bb3dc2ed4
Utkirbek Qutfiddinov <utkirbekqutfiddinov@gmail.com>
2025-12-03 08:48:15 +0500

    template class is ready

--- src/main/java/uz/utkirbek/aoc/Main.java
+++ src/main/java/uz/utkirbek/aoc/Main.java
@@ -1,8 +1,25 @@
 package uz.utkirbek.aoc;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        problem1();
+//        problem2();
     }
 
+    public static void problem1() throws IOException {
+//        String text="";
+//        String text=Files.readString(Path.of("text.txt"));
+        List<String> rows = Files.readAllLines(Path.of("text.txt"));
+    }
 
+    public static void problem2() throws IOException {
+//        String text="";
+//        String text=Files.readString(Path.of("text.txt"));
+        List<String> rows = Files.readAllLines(Path.of("text.txt"));
+    }
 }
